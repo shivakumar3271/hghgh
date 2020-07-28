2398075c77b3a8a76606a3eadf677b3b4bbdc680
shivakumar3271 <shivakumar3271@gmail.com>
2020-07-28 23:04:40 +0530

    second commit

--- src/test/java/com/pom/base/TestBase.java
+++ src/test/java/com/pom/base/TestBase.java
@@ -33,7 +33,7 @@ public class TestBase {
 	/*
 	 * WebDriver - done Properties - done Logs - log4j jar, .log,
 	 * log4j.properties, Logger ExtentReports DB Excel Mail ReportNG,
-	 * ExtentReports Jenkins
+	 * ExtentReports Jenkinsmnm
 	 * 
 	 */
 
